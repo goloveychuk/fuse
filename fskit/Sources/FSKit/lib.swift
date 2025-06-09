58905a9683d76b14d894127208e83c176b655c35
Vadym <goloveychuk1@gmail.com>
2025-06-09 15:09:36 +0000

    debug + fix

--- fskit/Sources/FSKit/lib.swift
+++ fskit/Sources/FSKit/lib.swift
@@ -264,7 +264,7 @@ extension FSItem {
         // Define the same constants that were previously enum cases
         public static let invalid = Identifier(rawValue: 0)!
         public static let parentOfRoot = Identifier(rawValue: 1)!
-        public static let rootDirectory = Identifier(rawValue: 2)! //todo not sure
+        public static let rootDirectory = Identifier(rawValue: 1)! //todo not sure
     }
 }
 
