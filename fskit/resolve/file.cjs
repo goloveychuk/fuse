0ec02c0f0647fc19d073593e4f2cd7ced0521e3b
Vadym <goloveychuk1@gmail.com>
2025-06-13 16:35:59 +0000

    clean

--- fskit/resolve/file.cjs
+++
@@ -1,9 +0,0 @@
-// import 'alias'
-// const {createRequire, registerHooks, register} = require('node:module');
-// const { fileURLToPath } = require('node:url');
-
-// require('./other')
-// register('./loader.js', __filename);
-// const require = createRequire(fileURLToPath(import.meta.url));
-
-console.log(require.resolve('alias'));
